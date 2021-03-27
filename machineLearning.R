f844bcde19f641768efb048b85e508903ee442e4
Synectome <jessedgb@gmail.com>
2021-03-27 15:20:24 -0400

    sadfasdf

--- machineLearning.R
+++ machineLearning.R
@@ -17,50 +17,113 @@ train_images_dir <- file.path(data_dir, 'TrainImages')
 train_images <- list.files(train_images_dir)
 
 test_images_dir <- file.path(data_dir, 'TestImages')
-test_images <- list.files(train_images_dir)
+test_images <- list.files(test_images_dir)
 
 #---------------------CROP TRAINING IMAGES --------------------
-train_image_crop <- function(image_dir, image_list){
-  for (image_name in image_list){
-    current_image_path <- file.path(image_dir, image_name)
-    image <- image_read(current_image_path)
-    image <- image_crop(image, "1024x840")
-    image_write(image, path = current_image_path, format = "png")
-  }
-}
-
-train_image_crop(train_images_dir, train_images)
+# https://cran.r-project.org/web/packages/magick/vignettes/intro.html#Read_and_write
+# train_image_crop <- function(image_dir, image_list){
+#   for (image_name in image_list){
+#     current_image_path <- file.path(image_dir, image_name)
+#     image <- image_read(current_image_path)
+#     image <- image_crop(image, "1024x840")
+#     image_write(image, path = current_image_path, format = "png")
+#   }
+# }
+# 
+# train_image_crop(train_images_dir, train_images)
 #-------------------------------------------------------------
 
+# https://www.shirin-glander.de/2018/06/keras_fruits/
+img_width <- 840
+img_height <- 1024
+target_size <- c(img_width, img_height)
+channels <- 1
 
-###########Load using tfdatasets
-# To load the files as a TensorFlow Dataset first create a dataset of the file paths:
-  
-list_ds <- file_list_dataset(file_pattern = paste0(data_dir, "/*/*"))
-list_ds %>% reticulate::as_iterator() %>% reticulate::iter_next()
+train_data_gen <- image_data_generator(
+  rescale = 1/255 #,
+  #rotation_range = 40,
+  #width_shift_range = 0.2,
+  #height_shift_range = 0.2,
+  #shear_range = 0.2,
+  #zoom_range = 0.2,
+  #horizontal_flip = TRUE,
+  #fill_mode = "nearest"
+)
+
+valid_data_gen <- image_data_generator(
+  rescale = 1/255
+) 
+
+#--load images into memory
+# https://www.rdocumentation.org/packages/keras/versions/2.3.0.0/topics/flow_images_from_directory
+# flow_images_from_directory(
+#   directory,
+#   generator = image_data_generator(),
+#   target_size = c(256, 256),
+#   color_mode = "grayscale",
+#   classes = NULL,
+#   class_mode = "categorical",
+#   batch_size = 32,
+#   shuffle = TRUE,
+#   seed = NULL,
+#   save_to_dir = NULL,
+#   save_prefix = "",
+#   save_format = "png",
+#   follow_links = FALSE,
+#   subset = NULL,
+#   interpolation = "nearest"
+# )
+# training images
+train_image_array_gen <- flow_images_from_directory(train_images_dir, 
+                                                    train_data_gen,
+                                                    target_size = target_size,
+                                                    class_mode = "categorical",
+                                                    color_mode = "grayscale",
+                                                    classes = NULL,
+                                                    seed = 42)#,
+                                                    # save_format = "png")
+
+# validation images
+valid_image_array_gen <- flow_images_from_directory(test_images_dir, 
+                                                    valid_data_gen,
+                                                    target_size = target_size,
+                                                    color_mode = "grayscale",
+                                                    class_mode = "categorical",
+                                                    classes = NULL,
+                                                    seed = 42)
+
+train_classes_indices <- train_image_array_gen$class_indices
+save(train_classes_indices, file = file.path(data_dir, "train_classes_indices.RData"))
+
+
+#----- Configuring Layers of the model ----
+model <- keras_model_sequential()
+model %>%
+  layer_flatten(input_shape = c(28, 28)) %>% # change the 2d array of the image into a 1d array to feed into the network
+  layer_dense(units = 2048, activation = 'relu') %>%
+  layer_dense(units = 4, activation = 'softmax') # corresponds to the number of classifications we want, For the xrays, itll be 4 of them?
+
+#----- Compile the model --------
+model %>% compile(
+  optimizer = 'adam',      # how the model is updated based on data and loss function
+  loss = 'sparse_categorical_crossentropy',   # loss function, want to minimize this to steer the model in the right direction
+  metrics = c('accuracy')  # used to monitor the training and testing steps
+)
+
+#------ Train the model on the data ------
+model %>% fit(train_images, train_labels, epochs = 5, verbose = 2)
+
+score <- model %>% evaluate(test_images, test_labels, verbose = 0) # using the test data
+# cat('Test loss:', score$loss, "\n")
+# cat('Test accuracy:', score$acc, "\n")
+print(score)
 
 
 
-get_label <- function(file_path) {
-  parts <- tf$strings$split(file_path, "/")
-  parts[-2] %>% 
-    tf$equal(classes) %>% 
-    tf$cast(dtype = tf$float32)
-}
 
-decode_img <- function(file_path, height = 1024, width = 840) {
-  size <- as.integer(c(height, width))
-  file_path %>% 
-    tf$io$read_file() %>% 
-    tf$image$decode_jpeg(channels = 3) %>% 
-    tf$image$convert_image_dtype(dtype = tf$float32) %>% 
-    tf$image$resize(size = size)
-}
 
-preprocess_path <- function(file_path) {
-  list(
-    decode_img(file_path),
-    get_label(file_path)
-  )
-}
+
+
+
+
 
